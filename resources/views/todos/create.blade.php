618da98b45bc8184ddf4807d8edfa6fb50bd61e8
Franck1Stein <franck.komla@gmail.com>
2020-08-09 10:49:53 +0000

    feature1: Form Validation start here

---
+++ resources/views/todos/create.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>Todos</title>
+</head>
+    <body>
+
+        <div class="text-center pt-10">
+
+            <h1 class="2xl">What next you need To-Do</h1>
+
+            <hr>
+
+            <form action="/todos/create" method="post"class="py-5">
+                @csrf
+
+                <input type="text" name="title" class="py-2 px-2 rounded-lg">
+
+                <input type="submit" value="Create" class="p-2 border rounded-lg">
+
+            </form>
+
+        </div>
+
+    </body>
+</html>
